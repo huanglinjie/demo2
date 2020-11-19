65b5c977dfbcd7e95f41d3b7e20a4e11d538fd08
黄林杰(80005148) <80005148@sf.com>
2020-11-19 14:26:40 +0800

    Initial commit

---
+++ base/demo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2020/11/10 15:37
+# @Author : 黄林杰
+# @File : demo.py
+# @Software: PyCharm
+
+
+import urllib.request
+from bs4 import BeautifulSoup
+request = urllib.request.urlopen('https://www.csdn.net/')
+soup = BeautifulSoup(request,'html.parser')
+print(type(soup))
+
+
